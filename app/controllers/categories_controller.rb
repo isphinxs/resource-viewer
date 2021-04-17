432abb477d96f85eea474dc56a127c37231af94c
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-17 15:03:27 -0400

    Add index and show views for Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,30 @@
 class CategoriesController < ApplicationController
+    def index
+        @categories = Category.all
+    end
+
+    def show
+        set_category
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_category
+        @category = Category.find_by(id: params[:id])
+    end
 end
