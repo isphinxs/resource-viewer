216f3c0916284359e924e8b997e14e8453194f10
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-17 19:34:56 -0400

    Add new and edit forms for Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,15 +8,27 @@ class CategoriesController < ApplicationController
     end
 
     def new
+        @category = Category.new
     end
 
     def create
+        @category = Category.new(category_params)
+        if @category.valid?
+            @category.save
+            redirect_to category_path(@category)
+        else
+            render :new
+        end
     end
 
     def edit
+        set_category
     end
 
     def update
+        set_category
+        @category.update(category_params)
+        redirect_to category_path(@category)
     end
 
     def destroy
@@ -26,5 +38,9 @@ class CategoriesController < ApplicationController
 
     def set_category
         @category = Category.find_by(id: params[:id])
+    end
+
+    def category_params
+        params.require(:category).permit(:name)
     end
 end
