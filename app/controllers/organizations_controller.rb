a4cd22181acf2d7abdc07a10b06ade10c5392bb8
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-17 14:58:55 -0400

    Add index and show views for Organization

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,2 +1,30 @@
 class OrganizationsController < ApplicationController
+    def index
+        @organizations = Organization.all
+    end
+
+    def show
+        set_organization
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+    
+    def destroy
+    end
+
+    private
+
+    def set_organization
+        @organization = Organization.find_by(id: params[:id])
+    end
 end
