bc9ec51f366335640399b66b0efff93a894025da
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-17 20:13:56 -0400

    Update .gitignore

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
     has_many :resource_categories
     has_many :resources, through: :resource_categories
+
+    validates :name, presence: :true, uniqueness: true
 end
