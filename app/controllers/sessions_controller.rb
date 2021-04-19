d5887396044c9146a9035603278f5d689ed368ee
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-19 19:26:30 -0400

    Add login and logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:username])
+    if !user.authenticate(params[:password])
+      render :new
+    else
+      session[:user_id] = user.id
+      redirect_to root_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
   end
 end
