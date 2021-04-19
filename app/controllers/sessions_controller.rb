fbea88b8f7f6196caf45f4b02b468ebf90af0793
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-19 19:57:58 -0400

    Add flash error message to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,18 @@
 class SessionsController < ApplicationController
   def new
+    if session[:user_id]
+      redirect_to root_path
+    end
   end
 
   def create
-    user = User.find_by(username: params[:username])
-    if !user.authenticate(params[:password])
-      render :new
-    else
-      session[:user_id] = user.id
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to root_path
+    else
+      flash[:alert] = "Invalid username or password."
+      redirect_to login_path
     end
   end
 
