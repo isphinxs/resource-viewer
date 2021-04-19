6d484750a170a56b8eea3b23094de4fa801d0137
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-19 19:09:33 -0400

    Add signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        # byebug
+        user = User.new(user_params)
+        if user.valid?
+            user.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   root "pages#home"
 
-  get "/signup", to: "sessions#new"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+
+  get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   post "/logout", to: "sessions#destroy"
-  
+
   resources :resources
   resources :organizations
   resources :categories
-  resources :users
+  resources :users, only: [:show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
