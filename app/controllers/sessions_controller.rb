ead5488020a7f00dee7ccb94c19d986146324be4
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-21 07:59:51 -0400

    Change login to form_with url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect_to root_path
     else
       flash[:alert] = "Invalid username or password."
