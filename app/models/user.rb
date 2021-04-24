76ef4b75dd5fa7d9f7e4cbd83902cc2f2d542a4e
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-24 13:16:43 -0400

    Validate minimum length for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     validates :first_name, :last_name, :username, :email, presence: true
     validates :username, uniqueness: true, on: :create
+    validates :password, length: { minimum: 8 }
 end
