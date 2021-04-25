6840dba57d4a2380eb70d69b1e5c46c0017090b2
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-25 15:27:15 -0400

    Clean up user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     validates :first_name, :last_name, :username, presence: true
     validates :username, uniqueness: true, on: :create
-    # validates :email, email: true
     validates :password, length: { minimum: 8 }
 
     include ActiveModel::Validations
