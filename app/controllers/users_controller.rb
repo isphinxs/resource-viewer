2008b0dd34988c968da1aef95fea33e50332d811
Samantha Ostrowski <isphinxs@hotmail.com>
2021-05-01 16:49:35 -0400

    Refactor User controller and correct sign up, edit, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
     before_action :require_authorization
     skip_before_action :require_authorization, only: [:new, :create]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
 
     def show
-        @user = current_user
         @ratings = @user.ratings.alphabetical_by_resource
-        byebug
-        # @ratings = @user.ratings
     end
 
     def new
@@ -14,10 +12,10 @@ class UsersController < ApplicationController
     end
 
     def create
-        # byebug
         @user = User.new(user_params)
         if @user.valid?
             @user.save
+            session[:user_id] = @user.id
             redirect_to root_path
         else
             render :new
@@ -25,15 +23,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find_by(id: session[:user_id])
     end
 
     def update
-        @user = current_user
-        if user_params[:password].blank?
-            user_params.delete(:password)
-            user_params.delete(:password_confirmation)
-        end
         if @user.update(user_params)
             redirect_to user_path(@user)
         else
@@ -42,8 +34,7 @@ class UsersController < ApplicationController
     end
 
     def destroy
-        # byebug
-        current_user.destroy
+        @user.destroy
         session[:user_id] = nil
         redirect_to root_path
     end
@@ -52,6 +43,10 @@ class UsersController < ApplicationController
     
     def require_authorization
         return head(:forbidden) unless current_user == User.find_by(id: params[:id])
+    end
+
+    def set_user
+        @user = current_user
     end
 
     def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :first_name, :last_name, :username, presence: true
     validates :username, uniqueness: true, on: :create
-    validates :password, length: { minimum: 8 }
+    validates :password, length: { minimum: 8 }, allow_nil: true
 
     include ActiveModel::Validations
     validates_with EmailValidator
