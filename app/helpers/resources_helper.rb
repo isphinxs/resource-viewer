eecbd2ee13dceea9c151e57ee7dd7ce01f6337de
Samantha Ostrowski <isphinxs@hotmail.com>
2021-05-01 23:12:06 -0400

    Create helper to generate a list of Resources

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,2 +1,12 @@
 module ResourcesHelper
+    def resource_list_index(object)
+        if object.resources.empty?
+            content_tag(:li, "No Resources Available")
+        else
+            object.resources.order(name: :asc).collect do |resource|
+                link = link_to(resource.name, resource_path(resource))
+                content_tag(:li, link)
+            end.join.html_safe
+        end
+    end
 end
