7063069d5eb284a3584257e30b01259c02d088e1
Samantha Ostrowski <isphinxs@hotmail.com>
2021-05-09 20:16:28 -0400

    Adjust number conversion for postgresql

--- db/migrate/20210425192854_change_amount_type_to_decimal.rb
+++ db/migrate/20210425192854_change_amount_type_to_decimal.rb
@@ -1,6 +1,6 @@
 class ChangeAmountTypeToDecimal < ActiveRecord::Migration[6.1]
   def up
-    change_column :resources, :amount, :decimal
+    change_column :resources, :amount, :decimal USING amount::numeric
   end
 
   def down
